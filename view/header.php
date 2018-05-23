5cb9124052bfba919b6e3a3e1f55ced3b001e018
Shizurah <eloise-martin@live.fr>
2018-05-23 16:54:04 +0200

    Création du fichier header.php

---
+++ view/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div>
+        <img src="../public/images/library2.png" alt="library"/>
+        <div id="titles">
+            <h1>Blog de Jean Forteroche</h1>
+            <h2>Ecrivain contemporain et romancier</h2>
+        </div>
+        <a href="#" id="connexion-btn">Connexion</a>
+    </div>
+</header>
