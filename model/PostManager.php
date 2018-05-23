64711b729903327689e4e75dcba9e01d7b49f114
Shizurah <eloise-martin@live.fr>
2018-05-23 14:31:30 +0200

    Ajout des méthodes get (post), getPostsList, update et delete dans la classe Post (fichier Post.php)

--- model/PostManager.php
+++ model/PostManager.php
@@ -3,30 +3,40 @@
 require_once('model/Manager.php');
 
 class PostManager extends Manager {
-    
-    public function createPost() {
-        $db = $this->dbConnect();
 
+    private $_db = $this->dbConnect();
+    
+    public function add(Post $post) { 
+
+        $req = $this->_db->prepare('INSERT INTO posts(title, postDate, content) VALUES(:post_title, NOW(), :post_content)'); 
+
+        $req->execute(array(
+            :post_title => $post->title(),
+            :post_content => $post->content()
+        ));
     }
 
     public function getPostsList() {
-        $db = $this->dbConnect();
-
+        $req = $this->_db->query('SELECT * FROM posts');
     }
 
-    public function getPost() {
-        $db = $this->dbConnect();
-
+    public function get($postId) {    
+        $req = $this->_db->query('SELECT * FROM posts WHERE id =' . $postId);
     }
 
-    public function updatePost() {
-        $db = $this->dbConnect();
+    public function update(Post $post) {       
 
+        $req = $this->_db->prepare('UPDATE posts SET title = :post_title, content = :post_content WHERE id = :post_id');
+
+        $req->execute(array(
+            :post_title => $post->title(),
+            :post_content => $post->content(),
+            :post_id => $post->id()
+        ));
     }
 
-    public function deletePost() {
-        $db = $this->dbConnect();
-        
+    public function delete(Post $post) {
+        $req = $this->_db->exec('DELETE FROM posts WHERE id =' . $post->id());
     }
     
 }
