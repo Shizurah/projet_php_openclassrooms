5f9790b8839a3d55b4f532d74aaf91621c862f15
Shizurah <eloise-martin@live.fr>
2018-05-23 16:55:40 +0200

    Stylisation du header du blog et inclusion du fichier correspondant ds fichier display_postsList.php

---
+++ view/display_postsList.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../public/css/style.css"/>
+    <title>Mon blog</title>
+</head>
+
+<body>
+
+<?php
+require_once('header.php');
+?>
+
+    
+</body>
+</html>
