730a91c3dfa75733964c639231ef3da4ecadb616
Shizurah <eloise-martin@live.fr>
2018-05-25 13:58:40 +0200

    Refactoring

--- view/aside.php
+++ view/aside.php
@@ -12,8 +12,6 @@
     <h3>CHAPITRES</h3>
     <?php
     foreach($posts as $post) {
-    // while ($data = $req1->fetch()) {
-    //     $post = new Post($data);
     ?>
     
     <a href="index.php?action=post&amp;postId=<?= $post->id() ?>" class="chapters-list"><?= $post->title() ?></a><br/>
