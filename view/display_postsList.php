f2c35effe25558bb4a13b1f08b7907944a05f133
Shizurah <eloise-martin@live.fr>
2018-05-29 21:43:49 +0200

    Changement de police bloc 'aside'

--- view/display_postsList.php
+++ view/display_postsList.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link href="https://fonts.googleapis.com/css?family=El+Messiri" rel="stylesheet"> 
     <link rel="stylesheet" href="public/css/style.css"/>
     <title>Mon blog</title>
 </head>
