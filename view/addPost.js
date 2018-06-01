1d61222d3b1ae010c49d4b2c1b24ba7f5b5d3a91
Shizurah <eloise-martin@live.fr>
2018-06-01 14:37:16 +0200

    Création fichier js pour affichage d'un message de succés à la publication d'un nouvel article + lien de redirection vers cet article

---
+++ view/addPost.js
@@ -0,0 +1,7 @@
+var chapterSubmitBtn = document.getElementById('chapter-submit-btn');
+var successMsg = document.getElementById('success-msg');
+
+
+chapterSubmitBtn.addEventListener('click', function() {
+    successMsg.style.display = 'visible';
+});
