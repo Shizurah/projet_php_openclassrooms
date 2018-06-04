4cdf233975e06c4fb80f4371656803c37d678471
Shizurah <eloise-martin@live.fr>
2018-06-04 13:17:14 +0200

    Renommage du fichier

--- view/comments.js
+++
@@ -1,7 +0,0 @@
-var writingCommentBtn = document.getElementById('writing-comment-btn');
-var sendingComentBtn = document.getElementById('sending-comment-btn');
-
-writingCommentBtn.addEventListener('click', function() {
-    writingCommentBtn.style.display = 'none';
-    document.getElementById('comment-form').style.display = 'block';
-});
