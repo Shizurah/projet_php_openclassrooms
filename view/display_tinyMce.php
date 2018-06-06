a50006334079db027e0623d6aa4869edb3fbc4e9
Shizurah <eloise-martin@live.fr>
2018-06-06 15:39:51 +0200

    Gestion de l'affichage d'un message de succès différent en fonction des paramètres de l'url

--- view/display_tinyMce.php
+++ view/display_tinyMce.php
@@ -7,14 +7,10 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
         <link href="https://fonts.googleapis.com/css?family=El+Messiri" rel="stylesheet">
-        
-        <?php
-            if (isset($_GET['action']) && $_GET['action'] == 'postUpdating') {
-        ?>
-            <link rel="stylesheet" href="public/css/style_management_space.css"/>
-            <title>Mon blog</title>
+        <link rel="stylesheet" href="public/css/style_management_space.css"/>
+        <title>Mon blog</title>
     </head>
-
+            
     <body>
 
         <div class="header-management-space">
@@ -32,27 +28,56 @@
             <a href="index.php?action=connexion" class="redirections-btns">Tableau de bord</a>    
         </div>
         
-        <div id="success-msg" style="display: none">
+        <div id="success-msg">
             <?php
-                if (isset($_GET['postId']) && $_GET['postId'] > 0) {
+                if ($_GET['action'] == 'updatedPost') {
             ?>
      
-                <p>
-                    Votre <span> chapitre </span> a bien été modifié ! <br/>  
-                </p>
+                    <p>
+                        Votre <span> chapitre </span> a bien été modifié ! <br/>  
+                    </p>
 
-                <a href="index.php?action=post&amp;postId=<?= $_GET['postId'] ?>">Voir le chapitre</a>
+                    <a href="index.php?action=post&amp;postId=<?= $_GET['postId'] ?>">Voir le chapitre</a>
+        
+            <?php
+                } elseif ($_GET['action'] == 'addedPost') {  
+            ?>   
+                    <p>
+                        Votre <span> chapitre </span> a bien été publié ! <br/>  
+                    </p>
+
+                    <a href="index.php?action=post&amp;postId=<?= $post->id() ?>">Voir le chapitre</a>
+            <?php
+                }
+            ?>
         </div>
+        
+        
+        <?php
+            if ($_GET['action'] == 'postWritting') {
+        ?>
+                <form action="index.php?action=addedPost" method="post">
+        <?php 
+            } elseif ($_GET['action'] == 'postUpdating') {
+        ?>
+                <form action="index.php?action=updatedPost&amp;postId=<?= $_GET['postId'] ?>" method="post">
         <?php
             }
         ?>
-        
-        
-        <form action="index.php?action=postUpdated" method="post">
+
+
             <label for="chapter-title">Titre</label><br/>
             <input  type="text" 
                     name="chapter-title" 
-                    value="<?= $post->title() ?>"
+
+                    <?php
+                        if (isset($_GET['action']) && $_GET['action'] == 'postUpdating') {
+                    ?>
+                        value="<?= $post->title() ?>"
+                    <?php
+                        }
+                    ?>
+
                     id="chapter-title" 
                     required 
             />
@@ -60,7 +85,13 @@
             <br/><br/>
 
             <textarea name="chapter-content" id="chapter-textarea" rows="30" required>
-                <?= $post->content(); ?>
+                <?php
+                    if (isset($_GET['action']) && $_GET['action'] == 'postUpdating') {
+                ?>
+                        <?= $post->content(); ?>
+                <?php
+                    }
+                ?>
             </textarea>
 
             <br/>
@@ -69,70 +100,6 @@
         </form>
 
         <script src="view/addPost.js"></script>
-
-
-
-
-        <?php
-            } else {
-        ?>
-            <link rel="stylesheet" href="../public/css/style_management_space.css"/>
-        
-            <title>Mon blog</title>
-        </head>
-
-        <body>
-
-            <div class="header-management-space">
-                <img src="../public/images/connexion_form" alt="library">
-            
-                <h2 id="tinyMce-page-title">
-                    RÉDIGER UN CHAPITRE
-                </h2>
-
-                <a href="../index.php?action=deconnexion" id="deconnexion-btn">Déconnexion</a>
-            </div>
-
-            <div id="redirections-btns">
-                <a href="../index.php?action=postsList" class="redirections-btns">Blog</a>
-                <a href="../index.php?action=connexion" class="redirections-btns">Tableau de bord</a>    
-            </div>
-            
-            <div id="success-msg">
-            <?php
-                if (isset($_GET['lastPost'])) {
-            ?>
-                    
-                    <p>
-                        Votre <span> chapitre </span> a bien été publié ! <br/>  
-                    </p>
-
-                    <a href="../index.php?action=post&amp;postId=<?= $_GET['lastPost'] ?>">Voir le chapitre</a>
-            </div>
-            <?php
-                }
-            ?>
-            
-            <form action="../index.php?action=addPost" method="post">
-                <label for="chapter-title">Titre</label><br/>
-                <input  type="text" name="chapter-title" id="chapter-title" required />
-                
-                <br/><br/>
-
-                <textarea name="chapter-content" id="chapter-textarea" rows="30" required>
-                </textarea>
-
-                <br/>
-                
-                <input type="submit" id="chapter-submit-btn" value="Publier" />
-            </form>
-
-            <script src="addPost.js"></script>
-        <?php
-            }
-        ?>
-
-
         
         <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
         
